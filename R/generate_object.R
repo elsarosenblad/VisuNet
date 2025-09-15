5de4f7fddcdae91c918414789275bec8ca67784b
elsarosenblad <elsa.rosenblads@gmail.com>
2025-09-15 08:42:28 +0200

    Normalization problem fixed
    Old code is still present as a ocmment below the new implementation

--- R/generate_object.R
+++ R/generate_object.R
@@ -1,28 +1,77 @@
-generate_object = function(decs, rules,type,  TopNodes, FiltrParam,  NodeColorType,  EdgeColor, EdgeWidth, NewDataNodes, NewDataEdges){
+generate_object = function(decs, rules, type, TopNodes, FiltrParam,
+                           NodeColorType, EdgeColor, EdgeWidth,
+                           NewDataNodes, NewDataEdges){
 
   AllNets = NULL
   Net = NULL
+
+  # --- Build per-decision networks ---
   for (i in decs){
-    #i = "control"
-    recRulesDecs = NULL
-    #rules for decision
-    RulesDec = rules[which(rules$decision ==i),]
-    #generate nodes and edges for decision
-    RulesSetSize=dim(RulesDec)[1]
-    if(RulesSetSize != 0){
-      Net = generateNet(i, RulesDec, type, RulesSetSize, TopNodes,FiltrParam,  NodeColorType = NodeColorType,EdgeColor, EdgeWidth, NewDataNodes, NewDataEdges)
-    }else{
-      Net = list(nodes = NULL, edges = NULL, NodeRulesSetPerNode = NULL)
+    RulesDec = rules[which(rules$decision == i), ]
+    RulesSetSize = nrow(RulesDec)
+    if (RulesSetSize != 0){
+      Net = generateNet(i, RulesDec, type, RulesSetSize, TopNodes, FiltrParam,
+                        NodeColorType = NodeColorType, EdgeColor, EdgeWidth,
+                        NewDataNodes, NewDataEdges)
+    } else {
+      Net = list(nodes = NULL, edges = NULL, RulesSetPerNode = NULL)
+    }
+    AllNets[[i]] = Net
+  }
+
+  # --- Build "all" by combining existing networks ---
+  if (length(decs) > 0) {
+    all_nodes <- NULL
+    all_edges <- NULL
+    all_rulesets <- list()
+
+    for (d in decs) {
+      if (!is.null(AllNets[[d]]$nodes)) {
+        all_nodes <- rbind(all_nodes, AllNets[[d]]$nodes)
+      }
+      if (!is.null(AllNets[[d]]$edges)) {
+        all_edges <- rbind(all_edges, AllNets[[d]]$edges)
+      }
+      if (!is.null(AllNets[[d]]$RulesSetPerNode)) {
+        all_rulesets <- c(all_rulesets, AllNets[[d]]$RulesSetPerNode)
+      }
     }
 
-    AllNets[[i]] = Net
-    Net =  NULL
+    AllNets[['all']] <- list(nodes = all_nodes,
+                             edges = all_edges,
+                             RulesSetPerNode = all_rulesets)
+  } else {
+    AllNets[['all']] <- list(nodes = NULL, edges = NULL, RulesSetPerNode = NULL)
   }
-  if(dim(rules)[1] != 0){
-    AllNets[['all']] = generateNet('all', rules, type, dim(recRulesFiltr)[1], TopNodes, FiltrParam,  NodeColorType = NodeColorType, EdgeColor, EdgeWidth,
-                                   NewDataNodes, NewDataEdges)
-  }else{
-    AllNets[['all']] = list(nodes = NULL, edges = NULL, NodeRulesSetPerNode = NULL)
-  }
+
   return(AllNets)
 }
+
+# generate_object = function(decs, rules,type,  TopNodes, FiltrParam,  NodeColorType,  EdgeColor, EdgeWidth, NewDataNodes, NewDataEdges){
+#   
+#   AllNets = NULL
+#   Net = NULL
+#   for (i in decs){
+#     #i = "control"
+#     recRulesDecs = NULL
+#     #rules for decision
+#     RulesDec = rules[which(rules$decision ==i),]
+#     #generate nodes and edges for decision
+#     RulesSetSize=dim(RulesDec)[1]
+#     if(RulesSetSize != 0){
+#       Net = generateNet(i, RulesDec, type, RulesSetSize, TopNodes,FiltrParam,  NodeColorType = NodeColorType,EdgeColor, EdgeWidth, NewDataNodes, NewDataEdges)
+#     }else{
+#       Net = list(nodes = NULL, edges = NULL, NodeRulesSetPerNode = NULL)
+#     }
+#     
+#     AllNets[[i]] = Net
+#     Net =  NULL
+#   }
+#   if(dim(rules)[1] != 0){
+#     AllNets[['all']] = generateNet('all', rules, type, dim(recRulesFiltr)[1], TopNodes, FiltrParam,  NodeColorType = NodeColorType, EdgeColor, EdgeWidth,
+#                                    NewDataNodes, NewDataEdges)
+#   }else{
+#     AllNets[['all']] = list(nodes = NULL, edges = NULL, NodeRulesSetPerNode = NULL)
+#   }
+#   return(AllNets)
+# }
