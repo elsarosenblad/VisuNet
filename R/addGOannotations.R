2b95c34973427b315c09364692fc6f8d4666e704
elsarosenblad <elsa.rosenblads@gmail.com>
2025-10-09 17:02:45 +0200

    Entrez ids

--- R/addGOannotations.R
+++ R/addGOannotations.R
@@ -4,4 +4,14 @@
 #' @return VisuNet object with GO annotations
 #' @export
 
-addGOannotations <- function(vis_object, ontology = "MF") {}
+addGOannotations <- function(vis_object, ontology = "MF") {
+    # get all genenames from network
+    gene_names <- vis_object$all$nodes$label
+    # convert gene symbols to entrez ids, "TP53" to "7157"
+    gene_entrez <- bitr(gene_names, fromType = "SYMBOL", toType = "ENTREZID", OrgDb = "org.Hs.eg.db")
+    # if no genes could be converted to entrez ids, use original network
+    if (nrow(gene_entrez) == 0) {
+        return(vis_object)
+    }
+    
+}
