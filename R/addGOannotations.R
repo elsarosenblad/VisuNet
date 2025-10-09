d402cebb838a6273827301f40a32d1153cd588df
elsarosenblad <elsa.rosenblads@gmail.com>
2025-10-09 16:58:17 +0200

    GO annotations function

---
+++ R/addGOannotations.R
@@ -0,0 +1,7 @@
+#' Add GO annotations to VisuNet nodes
+#' @param vis_object A VisuNet object
+#' @param ontology GO ontology (MF/BP/CC)
+#' @return VisuNet object with GO annotations
+#' @export
+
+addGOannotations <- function(vis_object, ontology = "MF") {}
