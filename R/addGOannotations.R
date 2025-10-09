bc921cadb7bf5eab0af97db1f2513c909a2b1d78
elsarosenblad <elsa.rosenblads@gmail.com>
2025-10-09 17:46:14 +0200

    libraries

--- R/addGOannotations.R
+++ R/addGOannotations.R
@@ -9,10 +9,15 @@ addGOannotations <- function(vis_object, ontology = "MF") {
         stop("clusterProfiler required")
     if (!requireNamespace("GO.db", quietly = TRUE))
         stop("GO.db required")
+
+    library(clusterProfiler)
+    library(GO.db)
+    library(org.Hs.eg.db)
+
     # get all genenames from network
     gene_names <- vis_object$all$nodes$label
     # convert gene symbols to entrez ids, "TP53" to "7157"
-    gene_entrez <- bitr(gene_names, fromType = "SYMBOL", toType = "ENTREZID", OrgDb = "org.Hs.eg.db")
+    gene_entrez <- bitr(gene_names, fromType = "SYMBOL", toType = "ENTREZID", OrgDb = org.Hs.eg.db)
     # if no genes could be converted to entrez ids, use original network
     if (nrow(gene_entrez) == 0) {
         return(vis_object)
@@ -47,4 +52,23 @@ addGOannotations <- function(vis_object, ontology = "MF") {
 
     # convert go_list to data frame
     go_df <- data.frame(label = names(go_list), GO_term = unlist(go_list), stringsAsFactors = FALSE)
+
+    nodes <- merge(vis_object$all$nodes, go_df, by = "label", all.x = TRUE)
+    nodes$title <- ifelse(!is.na(nodes$GO_term),
+                         paste0(nodes$title, '<br/>GO: <b>', ifelse(nodes$GO_term == "", "NA", nodes$GO_term), '</b>'),
+                         nodes$title)
+    vis_object$all$nodes <- nodes
+    for (dec in setdiff(names(vis_object), "all")) {
+        if (!is.null(vis_object[[dec]]$nodes) && nrow(vis_object[[dec]]$nodes) > 0) {
+            dec_nodes <- merge(vis_object[[dec]]$nodes, go_df, by = "label", all.x = TRUE)
+            dec_nodes$title <- ifelse(!is.na(dec_nodes$GO_term),
+                                     paste0(dec_nodes$title, '<br/>GO: <b>', ifelse(dec_nodes$GO_term == "", "NA", dec_nodes$GO_term), '</b>'),
+                                     dec_nodes$title)
+            vis_object[[dec]]$nodes <- dec_nodes
+        }
+    }
+
+    message("GO annotations added")
+    return(vis_object)
+
 }
