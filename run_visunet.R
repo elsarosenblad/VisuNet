025a8c305752e5b608f82a00dd46dab76b571ccd
elsarosenblad <elsa.rosenblads@gmail.com>
2025-10-13 08:41:17 +0300

    add annotations to visunet

--- run_visunet.R
+++ run_visunet.R
@@ -1,14 +1,9 @@
 library(R.ROSETTA)
 library(devtools)
 
-# load your development version of VisuNet
+# load version of VisuNet
 load_all("~/Documents/Forskningspraktik/VisuNet")
 
 ros <- rosetta(autcon)
-vis <- visunet(ros$main)
-
-# # # Original behavior (means)
-# vis_mean <- visunet(ros$main, agg_method = "mean")
-# 
-# # New behavior (sums for node size)
-# # vis_sum  <- visunet(ros$main, agg_method = "sum")
+# VisuNet with GO annotations
+vis <- visunet(ros$main, addGO = TRUE)
